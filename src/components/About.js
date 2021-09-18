027e356b8f93047f3ecfb66d3c45f6d23d459b16
Masatoshi <msts.oo0131@gmail.com>
2021-09-18 19:12:39 +0900

    props_css_compiled

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const style = {
+    color: "skyblue",
+    size: "30px",
+}
+
+
+
+const About = () => {
+    return (
+        <button style={style}>Welcome to our website!</button>
+    );
+};
+
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Home = () => {
-    return (
-        <h1>Welcome to our website!</h1>
-    );
+const style = {
+    minWidth: 64,
+    lineHeight: "32px",
+    borderRadius: 4,
+    border: "none",
+    padding: "0 16px",
+    color: "#fff",
+    background: "red"
 };
 
+class Home extends Component {
+    constructor(props) {
+        super();
+        this.title = props.title;
+        this.message = props.message;
+    }
+
+    render() {
+        return <div>
+            <h1>React</h1>
+            <p style={style}>{this.title}</p>
+            <p>{this.message}</p>
+        </div>;
+    }
+}
 export default Home ;
+
+// Reactでのcssは自分で開発する際はconst定義でとして取り出すことをメインとして書いてみる。
+// また、propsでの渡しに関しては、渡す先で渡したい文言等を書く。定義するファイル内ではぷrパティを渡せる枠組みを書くイメージ
