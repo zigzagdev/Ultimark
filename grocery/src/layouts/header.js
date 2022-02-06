0ec7c0ab999697a1a892285ff5d4b4bee75f1d3a
zigzagdev <msts.oo0131@gmail.com>
2022-02-06 23:35:31 +0900

    Header_fix

--- grocery/src/layouts/header.js
+++ grocery/src/layouts/header.js
@@ -2,21 +2,6 @@ import logo from '../logo.svg';
 import React from "react";
 import '../App.css';
 
-const style = {
-    backgroundColor:"black",
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-}
-
-const chrct = {
-    color: "white",
-    textAlign: "right",
-    paddingRight: "40px",
-}
-
-
-
 class Header extends React.Component {
     render() {
         return (
