24974c730b25087103b024ee52b357d029d3bf2b
zigzagdev <msts.oo0131@gmail.com>
2022-02-10 01:33:28 +0900

    initialcommit_newbranch

--- grocery/src/layouts/header.js
+++ grocery/src/layouts/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return (
             <div className="App-header">
-              FoodPage
+              EveryMenu
             </div>
         );
     }
