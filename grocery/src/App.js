880517e478bb2f9b0d9f182802d3a5c02d2a79f0
zigzagdev <msts.oo0131@gmail.com>
2022-02-12 13:43:01 +0900

    ItalianPage_Add

--- grocery/src/App.js
+++ grocery/src/App.js
@@ -2,28 +2,35 @@ import './App.css';
 import  Footer from  './layouts/Footer.tsx'
 import  Header from './layouts/Header'
 import  DarkMode from  './layouts/DarkMode.tsx'
-import react, {Components, Fragment} from 'react';
+import React, {Component} from 'react';
 import EachMenu from './components/EachMenu'
-import { BrowserRouter, Switch, Route, Link, useHistory } from "react-router-dom";
+import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
+
 import Japanese from './pages/Japanese';
 import French from './pages/French';
-import Main from './layouts/Main';
 
 
-function App() {
+const character = {
+  color: "white",
+}
+
+class App extends Component{
+  render() {
   return (
       <div className="Wrapper">
         <Header/>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/Japanese" component={Japanese}/>
-            <Route exact path="/French" component={French}/>
+            <Route exact path="/Japanese" component={Japanese}>
+            </Route>
+            <Route exact path="/French" component={French} />
           </Switch>
+          <a href="/Japanese" style={character}>Japanese</a>
         </BrowserRouter>
-        <Main/>
         <Footer/>
       </div>
-  );
+     );
+  }
 }
 
 export default App;
---
+++ grocery/src/pages/Italian.tsx
@@ -0,0 +1,50 @@
+import React, {Fragment} from 'react';
+
+
+const Italian1:  {[key: string]: string} = {
+    color: "white",
+    fontSize: "18px",
+}
+const style: { [key: string]: string } = {
+    marginTop: "auto",
+    width: "100%",
+    backgroundColor: "black",
+};
+
+const rightchrt: { [key: string]: string } = {
+    color: "white",
+    textAlign: "center",
+    paddingLeft: "60px",
+};
+
+const place:  {[key: string]: string} = {
+    display:"inline-block",
+    padding:  "20px 130px 20px ",
+    verticalAlign: "top",
+}
+
+const innerleft:  {[key: string]: string} = {
+    display: "block",
+}
+
+const right1:  {[key: string]: string} = {
+    color: "white",
+    fontSize: "24px",
+}
+const right2:  {[key: string]: string} = {
+    color: "white",
+    paddingTop:"10px",
+    fontSize: "18px",
+}
+
+class Italian extends React.Component {
+    render(){
+        return(
+           <Fragment>
+
+           </Fragment>
+        );
+    }
+}
+
+export default Italian ;
