f5724877905e7ce3e4e6e9894674b438b8404aae
zigzagdev <msts.oo0131@gmail.com>
2022-02-15 01:27:09 +0900

    btn_add

--- grocery/src/pages/Japanese.js
+++ grocery/src/pages/Japanese.js
@@ -26,18 +26,22 @@ const  cardboss = {
 const cardcomponent = {
   margin:'32px 0 18px 62px',
   flexWrap: 'wrap',
+  
 }
 
 const card = {
-  width:"15rem",
-  height:'280px',
+  width:"16rem",
+  height:'302px',
   backgroundColor:'white',
   tableLayout:'fixed',
+  display: 'flex',
+  flexDirection: 'column',
+  
 }
 
 const dishnameobj = {
   fontSize: '20px',
-  marginTop:'131px',
+  marginTop:'111px',
   color: 'darkblue',
 }
 
@@ -46,7 +50,11 @@ const contentobj = {
   wordBreak: 'break-all',
   overflowWrap: 'break-word',
   textAlign: 'left',
-  margin: '3px 11px 0 11px'
+  margin: '3px 13px 0 13px',
+}
+
+const btn = {
+  marginTop: 'auto',
 }
 
 export default function Japanese() {
@@ -75,6 +83,7 @@ export default function Japanese() {
                   <Card style={card}>
                     <h1 style={dishnameobj}>{dish.name}</h1>
                     <p style={contentobj}>{dish.content}</p>
+                    <Button style={btn}>Detail</Button>
                   </Card>
                 </Fragment>
               </div>
