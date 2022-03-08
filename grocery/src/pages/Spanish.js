f0466639cc0c2b2ce92ed336a4b7bb6e6de7d10d
zigzagdev <msts.oo0131@gmail.com>
2022-03-08 21:32:50 +0900

    SpanishPage_Header_add

--- grocery/src/pages/Spanish.js
+++ grocery/src/pages/Spanish.js
@@ -2,11 +2,22 @@ import React, {Fragment} from "react";
 import {spanish} from "../assets/dishes/assets";
 
 
+const outline = {
+  margin: "38px 0 148px 78px"
+}
+
+const  title = {
+  color: "white",
+  textAlign: "left",
+  fontSize: "30px",
+}
+
 console.log(spanish)
 function Spanish () {
   return(
-    <div style={{color:"white"}}>
-      ttttttttttttttttttttttt
+    <div style={outline}>
+      <h1 style={title}>Here are Spanish dishes Page .</h1>
+      
     </div>
   )
 }
