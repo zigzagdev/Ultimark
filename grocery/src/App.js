02ce2f45e6bed9998550b141f84b4d56ac80a42d
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2022-03-10 12:08:50 +0900

    3点リーダー

--- grocery/src/App.js
+++ grocery/src/App.js
@@ -1,37 +1,36 @@
-import React, { Components } from "react";
-import Header from  './layouts/Header'
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import Header from "./layouts/Header";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Japanese from "./pages/Japanese";
 import JapaneseDetail from "./pages/JapaneseDetail";
 import French from "./pages/French";
-import FrenchDetail from "./pages/FrenchDetail"
+import FrenchDetail from "./pages/FrenchDetail";
 import Main from "./layouts/Main";
 import Italian from "./pages/Italian";
-import ItalianDetail from  "./pages/ItalianDetail"
-import Footer from './layouts/Footer'
-import Form from './pages/Form'
-import './App.css'
-import Spanish from './pages/Spanish'
-
+import ItalianDetail from "./pages/ItalianDetail";
+import Footer from "./layouts/Footer";
+import Form from "./pages/Form";
+import "./App.css";
+import Spanish from "./pages/Spanish";
 
 function App() {
   return (
     <div className="Wrapper">
-      <Header/>
+      <Header />
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Main}/>
-          <Route exact path="/Japanese" component={Japanese}/>
-          <Route exact path="/Japanese/:id" component={JapaneseDetail}/>
-          <Route exact path="/French" component={French}/>
-          <Route exact path="/French/:id" component={FrenchDetail}/>
-          <Route exact path="/Italian" component={Italian}/>
-          <Route exact path="/Italian/:id" component={ItalianDetail}/>
-          <Route exact path="/Spanish" component={Spanish}/>
-          <Route exact path="/Form" component={Form}/>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/Japanese" component={Japanese} />
+          <Route exact path="/Japanese/:id" component={JapaneseDetail} />
+          <Route exact path="/French" component={French} />
+          <Route exact path="/French/:id" component={FrenchDetail} />
+          <Route exact path="/Italian" component={Italian} />
+          <Route exact path="/Italian/:id" component={ItalianDetail} />
+          <Route exact path="/Spanish" component={Spanish} />
+          <Route exact path="/Form" component={Form} />
         </Switch>
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- grocery/src/pages/Spanish.js
+++ grocery/src/pages/Spanish.js
@@ -1,88 +1,95 @@
-import React, {Fragment} from "react";
-import {spanish} from "../assets/dishes/assets";
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import {limit} from '../components/common/commonfunction'
-
+import React from "react";
+import { spanish } from "../assets/dishes/assets";
+import Card from "@material-ui/core/Card";
+import { limit } from "../components/common/commonfunction";
 
 const outline = {
-  margin: "38px 0 148px 78px"
-}
+  margin: "38px 0 148px 78px",
+};
 
 const title = {
   color: "white",
   textAlign: "left",
   fontSize: "30px",
-}
+};
 
-const  titledetail = {
+const titledetail = {
   fontSize: "27px",
   color: "snow",
   margin: "31px 0 0 66px",
-  textAlign: "left"
-}
+  textAlign: "left",
+};
 
 const cardboss = {
-  display: 'flex',
-  flexWrap: 'wrap'
-}
+  display: "flex",
+  flexWrap: "wrap",
+};
 
 const card = {
   width: "16rem",
-  height: '332px',
-  backgroundColor: 'white',
-  tableLayout: 'fixed',
-  display: 'flex',
-  flexDirection: 'column'
-}
+  height: "332px",
+  backgroundColor: "white",
+  tableLayout: "fixed",
+  display: "flex",
+  flexDirection: "column",
+};
 
-const  cardcomponent = {
-  margin: '32px 0 18px 62px',
-  flexWrap: 'wrap',
-}
+const cardcomponent = {
+  margin: "32px 0 18px 62px",
+  flexWrap: "wrap",
+};
 
-const sortby =
-  spanish.sort(function (a, b) {
-    if (a.price > b.price) return 1;
-    if (a.price < b.price) return -1;
-    return 0;
-  });
+const sortby = spanish.sort(function (a, b) {
+  if (a.price > b.price) return 1;
+  if (a.price < b.price) return -1;
+  return 0;
+});
 
 const dishobj = {
-  fontSize: '20px',
-  marginTop: '21px',
-  color: 'darkblue',
-}
+  fontSize: "20px",
+  marginTop: "21px",
+  color: "darkblue",
+};
 
 const dishnameobj = {
-  fontSize: '20px',
-  marginTop: '21px',
-  color: 'darkblue'
-}
+  fontSize: "20px",
+  color: "darkblue",
+  margin: "auto",
+  maxWidth: "200px",
+  display: "block",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+};
 
-function Spanish () {
-  return(
+function Spanish() {
+  return (
     <div style={outline}>
-      <div style={title}>
-        Spanish Page
-      </div>
+      <div style={title}>Spanish Page</div>
       <h1 style={titledetail}>Here are Spanish dishes Page .</h1>
       <div style={cardboss}>
-        {sortby.map(food => {
+        {sortby.map((food) => {
           return (
             <div style={cardcomponent}>
               <Card style={card}>
-                <img src={food.image} width={260} height={170}/>
-                <h1 style={dishnameobj}>{limit}{food.content}</h1>
+                <img
+                  src={food.image}
+                  alt={food.image}
+                  width={260}
+                  height={170}
+                />
+                <h1 style={dishnameobj}>
+                  {limit}
+                  {food.content}
+                </h1>
                 <h3 style={dishobj}>{food.price}</h3>
-                </Card>
-              </div>
-            )
-          })
-        }
+              </Card>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
 export default Spanish;
