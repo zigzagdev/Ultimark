a0b394f1f5126ed6cf8cd30f47bfbb557201b0a7
zigzagdev <msts.oo0131@gmail.com>
2022-03-16 20:36:08 +0900

    Question_flexbox_fix

--- grocery/src/layouts/Questions.js
+++ grocery/src/layouts/Questions.js
@@ -2,25 +2,45 @@ import react from  'react'
 import Card from "@material-ui/core/Card";
 
 const outline = {
-  margin: "58px 0 71px 127px",
+  margin: "58px 87px 71px 87px",
 }
 
 const middleline = {
-  margin: "21px, 0 38px 32px"
+  margin: "21px, 32px 38px 32px",
+  display: "flex",
+  justifyContent: "space-around",
+  flexWrap: "wrap"
 }
 
 const card = {
-  width: "16rem",
-  height: "332px",
+  width: "19rem",
+  height: "342px",
   backgroundColor: "white",
   tableLayout: "fixed",
-  display: "flex",
-  flexDirection: "column",
+  margin: "10px 10px 10px 10px",
+  flexWrap: "wrap",
+  flexDirection: 'column',
 }
 
 const cardstyle = {
   margin: "29px 8px 19px 8px"
 }
+
+const strong = {
+  fontSize:"12px"
+}
+
+const after = {
+  width: "19rem",
+  height: "342px",
+  backgroundColor: "black",
+  tableLayout: "fixed",
+  margin: "10px 10px 10px 10px",
+  flexWrap: "wrap",
+  flexDirection: 'column',
+}
+
+
 
 function Questions () {
   return(
@@ -28,7 +48,41 @@ function Questions () {
       <div style={middleline}>
         <Card style={card}>
           <div style={cardstyle}>
+            <strong style={strong}>
+              How can we find the meal which we want to order?
+            </strong>
           </div>
+        </Card>
+        <Card style={card}>
+          <div style={cardstyle}>
+            <strong style={strong}>
+              How can we find the meal which we want to order?
+            </strong>
+          </div>
+        </Card>
+        <Card style={card}>
+          <div style={cardstyle}>
+            <strong style={strong}>
+              How can we find the meal which we want to order?
+            </strong>
+          </div>
+        </Card>
+        <Card style={card}>
+          <div style={cardstyle}>
+            <strong style={strong}>
+            How can we find the meal which we want to order?
+          </strong>
+        </div>
+      </Card>
+        <Card style={card}>
+          <div style={cardstyle}>
+            <strong style={strong}>
+              How can we find the meal which we want to order?
+            </strong>
+          </div>
+        </Card>
+        <Card style={after}>
+        
         </Card>
       </div>
     </div>
