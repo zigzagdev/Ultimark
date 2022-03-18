841940e7a57b073d95388e41f0ee8eac740c9962
zigzagdev <msts.oo0131@gmail.com>
2022-03-18 23:20:54 +0900

    Toast_add

--- grocery/src/components/common/toast.js
+++ grocery/src/components/common/toast.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const gareth = {
   backgroundColor: "white",
@@ -12,8 +13,8 @@ function toastapp() {
   }
   return (
     <div>
-    <button onClick={notify} style={gareth}>Bale</button>
-    <ToastContainer />
+    <button onClick={notify}>Detail</button>
+    <ToastContainer/>
     </div>
   );
 }
